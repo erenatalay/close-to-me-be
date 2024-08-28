8ec8bd005237b38042aecefc49950bc8063bd02d
erenatalay <ahmetatalay95@gmail.com>
2024-08-28 14:53:05 +0300

    feature/i18next configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SwaggerModule } from './swagger/swagger.module';
-
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import { join } from 'path';
 @Module({
   imports: [
+    I18nModule.forRootAsync({
+      useFactory: () => ({
+        fallbackLanguage: 'en',
+        loaderOptions: {
+          path: join(__dirname, '/i18n/'),
+          watch: true,
+        },
+        typesOutputPath: join(__dirname, '../src/generated/i18n.generated.ts'),
+      }),
+      resolvers: [new HeaderResolver(['x-custom-lang'])],
+    }),
     AuthModule,
     UsersModule,
     PrismaModule,
